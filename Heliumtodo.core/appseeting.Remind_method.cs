56701fac4cd3110b2b525cd0ef775367ffd67eea
beigei_hei <1712190053@qq.com>
2026-07-30 23:06:05 +0800

    完善了核心依赖库

---
+++ Heliumtodo.core/appseeting.Remind_method.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heliumtodo.core
+{
+    public enum remind_method
+    {
+        usingsystem,
+        byheliumannounce
+
+    }
+    
+}
